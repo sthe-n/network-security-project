7f5502a6256cade70e9c437f44c8a279fd105b14
Lwandile Ntshangase <81806303+MsLwandile@users.noreply.github.com>
2026-09-03 11:04:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+    const report = "Introduction to Network Security.pdf";
+
+    document.getElementById("downloadpdf").addEventListener("click", function () {
+    const link = document.createElement("a");
+    link.href = report;
+    link.download = "Introduction to Network Security.pdf";
+    link.click();
+});
+
